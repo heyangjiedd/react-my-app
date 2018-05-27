0ea96bb4252a3733df1056f784a8abc865805b13
heyangjiedd <1059320200@qq.com>
2018-05-27 20:45:40 +0800

    简单路由

--- src/index.js
+++ src/index.js
@@ -1,31 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Redirect, Router, Route, Link,Switch} from 'react-router-dom';
-import { Provider } from 'react-router-redux';
+import {BrowserRouter, HashRouter, Redirect, Router, Route, Link, Switch} from 'react-router-dom';
 import './app.css';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import { DatePicker } from 'antd';
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
+function onChange(date, dateString) {
+    console.log(date, dateString);
+}
 class Home extends React.Component {
     render() {
-        return ( <div className="App">
-            <Switch>
-                <Route path="/login" exact render={(({location}) => (<h1>来玩啊，小帅哥</h1>)) } />
-                <Route path="/" render={(({location}) => (<h1>来玩啊，小帅哥321</h1>)) } />
-            </Switch>
+        return (<div>
+            <ul>
+                <li><Link to="/login">登录</Link></li>
+                <li><Link to="/register">注册</Link></li>
+            </ul>
+            <div>
+                <DatePicker onChange={onChange} />
+                <br />
+                <MonthPicker onChange={onChange} placeholder="Select month" />
+                <br />
+                <RangePicker onChange={onChange} />
+                <br />
+                <WeekPicker onChange={onChange} placeholder="Select week" />
+            </div>
         </div>)
     }
 }
+
 class LoginContainer extends React.Component {
     render() {
-        return (<div>niha</div>)
+        return (<div>登录</div>)
     }
 }
-ReactDOM.render(<BrowserRouter>
-    <Provider>
-        <div>
-            <Switch>
-                <Route exact path="/" component={LoginContainer}/>
-                <Route path="/home" component={Home}/>
-                <Redirect to="/"/>
-            </Switch>
-        </div>
-    </Provider>
-</BrowserRouter>, document.getElementById('root'));
+
+class Register extends React.Component {
+    render() {
+        return (<div>注册</div>)
+    }
+}
+
+ReactDOM.render(
+    <LocaleProvider locale={zhCN}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/login" component={LoginContainer}/>
+                    <Route path="/register" component={Register}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </LocaleProvider>
+            , document.getElementById('root'));
